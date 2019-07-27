a0e4d7532203817931b928540194578044ca4950
Roland J Jarvis <Rjarvis87@gmail.com>
2019-07-27 14:11:42 -0400

    Specials ready to go; ready for Step4

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -3,11 +3,11 @@ import React from "react";
 const OperatorButton = props => {
   const operator = props.operator;
   return (
-    <>
+    <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */
         operator
       }
-    </>
+    </button>
   );
 };
 
