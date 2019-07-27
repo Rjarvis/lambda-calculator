af021eff1473123cb1f00c7d54f9ea90ac05a7aa
Roland J Jarvis <Rjarvis87@gmail.com>
2019-07-27 14:01:56 -0400

    Data ready for export.  numbers ready for div pack

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,8 +4,7 @@ const NumberButton = props => {
   const number = props.number;
   return (
     <button>
-      /* Display a button element rendering the data being passed down from the parent container on props */
-      {number}
+      {number}{/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
   );
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,23 +1,25 @@
 import React from "react";
-
+//like StudentsList in Brian's code-pen;
 //import any components needed
 import {NumberButton} from "./NumberButton";
 //Import your array data to from the provided data file
-import {numbers} from ".data";
+
 
 const Numbers = props => {
   // STEP 2 - add the imported data to state
-  number = props.numbers;
+  /* STEP 3 - Use .map() to iterate over your array data and return a button 
+    component matching the name on the provided file. Pass
+    it any props needed by the child component*/
   return (
     <div>
-       {/* STEP 3 - Use .map() to iterate over your array data and return a button 
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       
       {
-        number.map(element =>{
-          return NumberButton(element);
+        props.numbers.map(numberElement =>{
+          return <NumberButton number={numberElement}/>;
         })
        }
     </div>
   );
 };
+
+export default Numbers;
