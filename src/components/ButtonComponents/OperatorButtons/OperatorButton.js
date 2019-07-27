0f3c2a81fcde67d2508c39832317c5890bccdb18
Roland J Jarvis <Rjarvis87@gmail.com>
2019-07-27 14:05:35 -0400

    Operators set for packing

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = props => {
+  const operator = props.operator;
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+        operator
+      }
     </>
   );
 };
+
+export default OperatorButton;
